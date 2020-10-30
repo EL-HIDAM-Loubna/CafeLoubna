43ba78ef394e5d1e2e939133360ced9f8aa9b66f
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-10-30 19:16:57 +0100

    my app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# App3
+# CafeLoubna
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.29.
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CuisineComponent } from './components/cuisine/cuisine.component';
+import { PatisserieComponent } from './components/patisserie/patisserie.component';
+import { SaladeComponent } from './components/salade/salade.component';
+import { ShowallComponent } from './components/showall/showall.component';
+import { LoginComponent } from './contact/login/login.component';
+import { RegisterComponent } from './contact/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component: ShowallComponent},
+  {path:"patisserie" , component: PatisserieComponent},
+  {path:"cuisine" , component: CuisineComponent},
+  {path:"salade" , component: SaladeComponent},
+  {path:"login" , component: LoginComponent},
+  {path:"register" , component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
